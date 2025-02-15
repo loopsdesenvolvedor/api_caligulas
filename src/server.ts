a42c187c3d397a0ee93c6493e98fc3d3889314c4
Elizeu Lopes <scarabellot.lopes@gmail.com>
2025-02-15 00:13:05 -0300

    feat: integrate express-async-errors for automatic error handling

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import cors from "cors";
 
 const PORT = process.env.PORT;
@@ -7,7 +9,21 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api", routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on port: ${PORT}`);
