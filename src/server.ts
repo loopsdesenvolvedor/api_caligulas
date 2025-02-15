85bc4c64d5b814beebd66a9ddad40fe04a8b5490
Elizeu Lopes <scarabellot.lopes@gmail.com>
2025-02-15 00:12:18 -0300

    feat(routes): add initial configuration for routes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import cors from "cors";
 
 const PORT = process.env.PORT;
+import { routes } from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api", routes);
 
 app.listen(PORT, () => {
   console.log(`App is running on port: ${PORT}`);
